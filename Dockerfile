0c2bf3177bd78c4938ffcb6b2c3ea149c74e0265
Mor Gilad <morg@wix.com>
2018-07-08 15:45:18 +0300

    base commit

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM docker-repo.wixpress.com/wix-bootstrap-onbuild:stable
+MAINTAINER Mor Gilad <morg@wix.com>
---
+++ test/e2e/e2e-common.js
@@ -0,0 +1,9 @@
+import puppeteer from 'puppeteer';
+
+before(async () => {
+  global.browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+});
+
+after(async () => {
+  await global.browser.close();
+});
