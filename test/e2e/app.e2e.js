2e408f7aeafd93a509c79269b2b61e9e665f8a5b
Mor Gilad <morg@wix.com>
2018-07-10 18:06:31 +0300

    testing

--- test/e2e/app.e2e.js
+++ test/e2e/app.e2e.js
@@ -4,6 +4,10 @@ describe('React application', () => {
   describe('open page', () => {
     eyes.it('should display title', async () => {
       await browser.get('/');
+      console.log('process.env.APPLITOOLS_GITHUB_FT');
+      console.log(process.env.APPLITOOLS_GITHUB_FT);
+      console.log('process.env.APPLITOOLS_BATCH_ID');
+      console.log(process.env.APPLITOOLS_BATCH_ID);
       expect($('h2').getText()).toEqual('b6 title');
     });
   });
