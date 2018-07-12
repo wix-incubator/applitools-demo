5ceba1d57d59b7c2dfc1fa35af3e8a1fe1928bea
Mor Gilad <morg@wix.com>
2018-07-12 10:47:05 +0300

    cange back to master title

--- test/e2e/app.e2e.js
+++ test/e2e/app.e2e.js
@@ -4,7 +4,7 @@ describe('React application', () => {
   describe('open page', () => {
     eyes.it('should display title', async () => {
       await browser.get('/');
-      expect($('h2').getText()).toEqual('b10 title');
+      expect($('h2').getText()).toEqual('master title');
     });
   });
 });
