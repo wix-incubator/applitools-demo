e0f61638e57f903301ea4222cd814c76dbe575c7
Mor Gilad <morg@wix.com>
2018-07-14 09:28:19 +0300

    test

--- test/e2e/app.e2e.js
+++ test/e2e/app.e2e.js
@@ -3,6 +3,11 @@ import eyes from 'eyes.it';
 describe('React application', () => {
   describe('open page', () => {
     eyes.it('should display title', async () => {
+      console.log('process.env.APPLITOOLS_GITHUB_FT');
+      console.log(process.env.APPLITOOLS_GITHUB_FT);
+      console.log(process.env.APPLITOOLS_BATCH_ID);
+      console.log('process.env.EYES_BATCH_UUID');
+      console.log(process.env.EYES_BATCH_UUID);
       await browser.get('/');
       expect($('h2').getText()).toEqual('b2 title');
     });
